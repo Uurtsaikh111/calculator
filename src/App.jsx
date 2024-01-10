441e08058326be3e6c3fb9d682cf2563f6a93e24
Үүрцайх Норовсамбуу <you@example.com>
2024-01-09 20:29:33 -0800

    Jan10M

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-
 import Header from "./components/Header";
-
 import "./style/global.css";
 
 export default function App() {
   return (
     <div>
       <Header />
-      
     </div>
   );
 }
